2932fc470e319989c75fedace320da866142355d
cashweaver <cashbweaver@gmail.com>
2020-04-02 09:38:33 -0700

    Calculate option chain prorit in another fn

--- option_chain_profit/example_usage.py
+++ option_chain_profit/example_usage.py
@@ -1,17 +1,43 @@
-from calc_option_chain_profit import calc_option_chain_profit
+from get_option_chain import get_option_chain
 from ib_insync import IB, Stock
+import pandas as pd
+from typing import List
+
+def calc_option_contract_profit(
+    asks: List[float],
+    strikes: List[float],
+    multipliers: List[int],
+    rights: List[str],
+    future_price: float,
+) -> List[float]:
+    right_is_call = rights.apply(lambda right: 1 if right == "C" or right == "CALL" else -1)
+    return ((-1.0 * asks) + (right_is_call * (future_price - strikes))) * multipliers
+
+pd.set_option("display.max_rows", None)
 
 ib = IB()
 ib.connect()
 
-spy = Stock("SPY", "SMART", "USD")
-ib.reqMarketDataType(3)
-ib.qualifyContracts(spy)
+contract = Stock("SPY", "SMART", "USD")
+ib.reqMarketDataType(1)
+ib.qualifyContracts(contract)
 
-df = calc_option_chain_profit(ib, spy, ["20200417"], 220.0)
-df = df.sort_values(by=["ProfitPerCostDollar"], ascending=False)
+future_prices = [240.0, 230.0, 220.0]
+expirations = ["20200417"]
+option_chain = get_option_chain(ib, contract, expirations, strike_min=min(future_prices))
 
-print("\n\n")
-print(df.head(10))
+for future_price in future_prices:
+    df = option_chain.copy()
+    print("\n\n")
+    print(f"For: {contract.symbol} with future price of {future_price}")
+    df["Price"] = df["Ask"] * df["Multiplier"]
+    df["ProfitPerContract"] = calc_option_contract_profit(df["Ask"], df["Strike"], df["Multiplier"], df["Right"], future_price)
+    df["ProfitPerDollar"] = df["ProfitPerContract"] / (df["Ask"] * df["Multiplier"])
+    df.drop(columns=["Multiplier"])
+    df = df.sort_values(by=["ProfitPerDollar"], ascending=False)
+
+    by_expiration = df.groupby("Expiration")
+    for expiration, frame in by_expiration:
+        print(frame.head(15), end="\n\n")
 
 ib.disconnect()
---
+++ option_chain_profit/get_option_chain.py
@@ -0,0 +1,51 @@
+from ib_insync import IB, Option, Contract
+import pandas as pd
+
+def get_option_chain(
+    ib: IB,
+    qualified_contract: Contract,
+    expirations: str,
+    use_delayed_data=False,
+    strike_min=None,
+    strike_max=None,
+    rights=["P", "C"],
+) -> pd.DataFrame:
+    """
+    TODO: Write documentation
+    """
+
+    if use_delayed_data:
+        ib.reqMarketDataType(3)
+    [ticker] = ib.reqTickers(qualified_contract)
+    current_price = ticker.marketPrice()
+    strike_min = strike_min or current_price * 0.90
+    strike_max = strike_max or current_price * 1.10
+
+    chains = ib.reqSecDefOptParams(qualified_contract.symbol, '', qualified_contract.secType, qualified_contract.conId)
+    chain = next(c for c in chains
+                 if c.tradingClass == qualified_contract.symbol
+                 and c.exchange == qualified_contract.exchange)
+    strikes = [strike for strike in chain.strikes
+               if strike_min < strike < strike_max]
+    contracts = [Option(qualified_contract.symbol, expiration, strike, right, qualified_contract.exchange, tradingClass=qualified_contract.symbol)
+            for right in rights
+            for expiration in expirations
+            for strike in strikes]
+
+    if use_delayed_data:
+        ib.reqMarketDataType(3)
+    ib.qualifyContracts(*contracts)
+    contracts = [contract for contract in contracts if contract.multiplier]
+
+    if use_delayed_data:
+        ib.reqMarketDataType(3)
+    tickers = ib.reqTickers(*contracts)
+
+    d = {
+        "Expiration": [str(ticker.contract.lastTradeDateOrContractMonth) for ticker in tickers],
+        "Strike": [ticker.contract.strike for ticker in tickers],
+        "Right": [str(ticker.contract.right) for ticker in tickers],
+        "Ask": [ticker.ask for ticker in tickers],
+        "Multiplier": [int(ticker.contract.multiplier) for ticker in tickers],
+        }
+    return pd.DataFrame(data=d)
